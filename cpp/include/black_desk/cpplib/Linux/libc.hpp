bf54a4ce12db285eef98809fc8332488bb62b632
black-desk <me@black-desk.cn>
2022-12-09 00:06:14 +0800

    feat(cpp): add wrapper for realpath

---
+++ cpp/include/black_desk/cpplib/Linux/libc.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <filesystem>
+
+namespace black_desk::cpplib::Linux
+{
+
+class FileDescriptorHolder;
+
+namespace libc
+{
+
+auto realpath(const std::filesystem::path &path) -> std::filesystem::path;
+auto realpath(const FileDescriptorHolder &fd_holder) -> std::filesystem::path;
+
+}
+
+} // namespace black_desk::cpplib::Linux
