7bc1f363ef3779a4d729ceb6d245b2df0e0e5b46
black-desk <me@black-desk.cn>
2022-12-12 21:29:42 +0800

    fix(cpp): add exception macro

--- cpp/tests/error_test.cpp
+++ cpp/tests/error_test.cpp
@@ -33,8 +33,7 @@ TEST(Common, nested_expection_wrap) // NOLINT
         try {
                 try { // 1
                         try { // 2
-                                std::string empty_string;
-                                empty_string.at(1);
+                                EXCEPTION("exception here");
                         } catch (...) {
                                 NESTED_EXCEPTION("try 2 failed");
                         }
@@ -43,17 +42,22 @@ TEST(Common, nested_expection_wrap) // NOLINT
                 }
         } catch (const std::exception &exception) {
                 std::string what = fmt::format("{}", exception);
-                ASSERT_NE(what.find("42"), std::string::npos)
+                EXPECT_NE(what.find("41"), std::string::npos)
                         << "should found line number of try 1";
-                ASSERT_NE(what.find("try 1 failed"), std::string::npos)
+                EXPECT_NE(what.find("try 1 failed"), std::string::npos)
                         << "should found try 1 message";
 
-                ASSERT_NE(what.find("39"), std::string::npos)
+                EXPECT_NE(what.find("38"), std::string::npos)
                         << "should found line number of try 2";
-                ASSERT_NE(what.find("try 2 failed"), std::string::npos)
+                EXPECT_NE(what.find("try 2 failed"), std::string::npos)
                         << "should found try 2 message";
 
-                ASSERT_NE(what.find("error_test.cpp"), std::string::npos)
+                EXPECT_NE(what.find("36"), std::string::npos)
+                        << "should found line number of exception";
+                EXPECT_NE(what.find("exception here"), std::string::npos)
+                        << "should found exception message";
+
+                EXPECT_NE(what.find("error_test.cpp"), std::string::npos)
                         << "should found file name";
         }
 }
