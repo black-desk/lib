4cf463679e97fc2a2b8077d915f82cd5a5a7d12e
black-desk <me@black-desk.cn>
2023-01-02 16:45:06 +0800

    feat(cpp): add strings::split
    * Move join to namespace "strings"
    * Add strings::split

--- cpp/include/black_desk/cpplib/utils.hpp
+++ cpp/include/black_desk/cpplib/utils.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 #include <fmt/format.h>
-namespace black_desk::cpplib
+namespace black_desk::cpplib::strings
 {
 
 inline auto join(const std::vector<std::string> &strs,
@@ -13,4 +13,25 @@ inline auto join(const std::vector<std::string> &strs,
         return fmt::format("{}", fmt::join(strs.begin(), strs.end(), sep));
 }
 
+template <typename T>
+auto split(const std::string &input, const T &delimiter)
+        -> std::vector<std::string>
+{
+        std::string delim = delimiter;
+        size_t pos_start = 0;
+        size_t pos_end = -1;
+        size_t delim_len = delim.length();
+        std::string token;
+        std::vector<std::string> res;
+
+        while ((pos_end = input.find(delim, pos_start)) != std::string::npos) {
+                token = input.substr(pos_start, pos_end - pos_start);
+                pos_start = pos_end + delim_len;
+                res.push_back(token);
+        }
+
+        res.push_back(input.substr(pos_start));
+        return res;
 }
+
+} // namespace black_desk::cpplib::strings
