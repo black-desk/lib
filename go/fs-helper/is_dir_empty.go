3770eb135f3fff738a8001f8c72560b27d5eb0aa
black-desk <me@black-desk.cn>
2023-01-09 13:58:36 +0800

    feat(go): add fshelper

---
+++ go/fs-helper/is_dir_empty.go
@@ -0,0 +1,21 @@
+package fshelper
+
+import (
+	"io"
+	"os"
+)
+
+// https://stackoverflow.com/a/30708914
+func IsDirEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1) // Or f.Readdir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
+}
