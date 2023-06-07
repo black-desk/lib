9c2aefaa7ff3cadf9a723d62e91bf7e763e999d3
black-desk <me@black-desk.cn>
2023-06-08 02:02:45 +0800

    style(errwrap)!: remove Trace
    Signed-off-by: black-desk <me@black-desk.cn>

--- go/errwrap/wrap.go
+++ go/errwrap/wrap.go
@@ -5,21 +5,6 @@ import (
 	"runtime"
 )
 
-func Trace(err error, annotate ...any) error {
-	if err == nil {
-		return nil
-	}
-
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		panic("failed to get caller")
-	}
-
-	msg := formatAnnotate(annotate)
-
-	return fmt.Errorf("%s:%d"+msg+"\n%w", file, line, err)
-}
-
 func formatAnnotate(annotate []any) string {
 	if len(annotate) == 0 {
 		return ""
